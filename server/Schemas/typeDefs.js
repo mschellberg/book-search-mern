53865f86efa0972fa0b7464034a4dbb370ae4b52
Melanie <mschellberg27@icloud.com>
2021-04-09 16:53:45 -0400

    first commit

---
+++ server/Schemas/typeDefs.js
@@ -0,0 +1,36 @@
+// import gql template function
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql `
+    type Query {
+        me: User
+    },
+    type Mutation {
+        login(email: String!, password: String!) : Auth
+        addUser(username: String!, email: String!, password: String!) : Auth
+        saveBook:
+        removeBook:
+    },
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    },
+    type Book {
+        _id: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+
+    },
+    type Auth {
+        token: ID
+        user: User
+    }
+    
+`
+module.exports = typeDefs;
